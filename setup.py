4eedadf9a15938b73a0101a669c2e8eb38d51343
Marian Graurov <marian.graurov@icbdigital.no>
2023-10-04 11:29:09 +0300

    Remove unnecessary dependencies

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def requirements_from_file(filename="requirements.txt"):
 
 
 setup(
-    version="1.0.4",
+    version="1.0.5",
     cmdclass={
         "build_py": unasync.cmdclass_build_py(
             rules=[

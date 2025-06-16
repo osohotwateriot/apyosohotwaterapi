1c2b49effa99a1ea5649b0c6abdf261af1a354b1
osohotwateriot <osowh.iot@gmail.com>
2025-06-16 13:33:19 +0300

    Bump version to 1.1.5

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def requirements_from_file(filename="requirements.txt"):
 
 
 setup(
-    version="1.1.4",
+    version="1.1.5",
     cmdclass={
         "build_py": unasync.cmdclass_build_py(
             rules=[
